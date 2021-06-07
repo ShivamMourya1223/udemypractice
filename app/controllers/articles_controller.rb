4a35d8d2bb287045d40573e00dc8b3d1f99219ba
Shivam Mourya <shivamyuvasoft310@gmail.com>
2021-06-07 10:07:57 +0530

    All clean

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,54 @@
+class ArticlesController < ApplicationController
+
+    before_action :set_articles,only:[:show,:edit,:update,:destroy]
+
+	def index
+		@articles = Article.all
+	end
+
+	def show
+	end
+
+	def new
+    @article = Article.new
+  end
+
+  def create
+    	@article = Article.create(article_params)
+         if @article.save
+          flash[:notice] = "Article was save successfully"
+    	       redirect_to article_path(@article)
+           else
+             render 'new'
+          end
+  end
+
+    def edit
+    end
+
+    def update
+    		@article.update(article_params)
+          if @article.save
+            flash[:notice] = "Article was successfully Updated"
+    	      redirect_to article_path(@article)
+          else
+            render'edit'
+          end
+
+    end
+
+      def destroy
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    private
+
+    def set_articles
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title,:description)
+    end
+end
---
+++ db/migrate/20210602145739_add_to_articles.rb
@@ -0,0 +1,6 @@
+class AddToArticles < ActiveRecord::Migration[6.1]
+  def change
+    add_column :articles,:description,:string
+     
+  end
+end
